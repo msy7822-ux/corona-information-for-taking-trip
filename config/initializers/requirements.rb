eb6fca908a4b6fa7a607a72b161b07519ea42907
masahiro fukatsu <masa.1108@icloud.com>
2021-03-17 22:33:07 +0900

    Railsにmoduleファイルの読み込みをきちんと行わせる

---
+++ config/initializers/requirements.rb
@@ -0,0 +1,5 @@
+require Rails.root.join('lib/utils/google_api_methods')
+require Rails.root.join('lib/utils/corona_api_methods')
+require Rails.root.join('lib/utils/linebot_api_methods')
+require Rails.root.join('lib/utils/physical_condition')
+require Rails.root.join('lib/utils/flex_message')
